84e56511f80414d98380de8d7dc28134932097c1
AlindaB <alindabyamukama@gmail.com>
2022-05-04 14:53:39 +0300

    JS: created function for volume-bar mute/unmute - changes volume - returns to previous place or default - changes volume-icon style

--- script.js
+++ script.js
@@ -62,6 +62,8 @@ function setProgress(e) {
 
 // Volume Controls --------------------------- //
 
+let lastVolume = 1
+
 // Volume Bar
 function changeVolume(e) {
     let volume = e.offsetX / volumeRange.offsetWidth
@@ -84,6 +86,24 @@ function changeVolume(e) {
     } else if (volume === 0) {
         volumeIcon.classList.add('fas', 'fa-volume-off')
     }
+    lastVolume = volume
+}
+
+// Mute/Unmute
+function toggleMute() {
+    volumeIcon.classList = ''
+    if (video.volume) {
+        lastVolume = video.volume
+        video.volume = 0
+        volumeBar.style.width = 0
+        volumeIcon.classList.add('fas', 'fa-volume-mute')
+        volumeIcon.setAttribute('title', 'Unmute')
+    } else {
+        video.volume = lastVolume
+        volumeBar.style.width = `${lastVolume * 100}%`
+        volumeIcon.classList.add('fas', 'fa-volume-up')
+        volumeIcon.setAttribute('title', 'Mute')
+    }
 }
 
 
@@ -100,3 +120,4 @@ video.addEventListener('timeupdate', updateProgress)
 video.addEventListener('canplay', updateProgress)
 progressRange.addEventListener('click', setProgress)
 volumeRange.addEventListener('click', changeVolume)
+volumeIcon.addEventListener('click', toggleMute)
