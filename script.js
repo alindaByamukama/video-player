6b9f3d2087e34263db2598686120e143182c7702
AlindaB <alindabyamukama@gmail.com>
2022-05-04 15:23:14 +0300

    JS: created function to change video-playback-speed - increases or decrease video speed according to value selected

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ const volumeBar = document.querySelector('.volume-bar')
 const currentTime = document.querySelector('.time-elapsed')
 const duration = document.querySelector('.time-duration')
 const fullscreenBtn = document.querySelector('.fullscreen')
+const speed = document.querySelector('.player-speed')
 
 // Play & Pause ----------------------------------- //
 
@@ -109,6 +110,11 @@ function toggleMute() {
 
 // Change Playback Speed -------------------- //
 
+function changeSpeed() {
+    video.playbackRate = speed.value
+//    console.log('video playback rate', video.playbackRate)
+//    console.log('selected value', speed.value)
+}
 
 
 // Fullscreen ------------------------------- //
@@ -121,3 +127,4 @@ video.addEventListener('canplay', updateProgress)
 progressRange.addEventListener('click', setProgress)
 volumeRange.addEventListener('click', changeVolume)
 volumeIcon.addEventListener('click', toggleMute)
+speed.addEventListener('change', changeSpeed)
