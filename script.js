588d19e7966d66ead65c0edd9b88edfe214a6550
AlindaB <alindabyamukama@gmail.com>
2022-05-04 15:42:00 +0300

    JS: created fullscreen functionailty with help from w3c reference

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const player = document.querySelector('.player')
 const video = document.querySelector('video')
 const progressRange = document.querySelector('.progress-range')
 const progressBar = document.querySelector('.progress-bar')
@@ -119,6 +120,42 @@ function changeSpeed() {
 
 // Fullscreen ------------------------------- //
 
+/* View in fullscreen */
+function openFullscreen(elem) {
+    if (elem.requestFullscreen) {
+      elem.requestFullscreen();
+    } else if (elem.webkitRequestFullscreen) { /* Safari */
+      elem.webkitRequestFullscreen();
+    } else if (elem.msRequestFullscreen) { /* IE11 */
+      elem.msRequestFullscreen();
+    }
+    video.classList.add('video-fullscreen')
+  }
+  
+  /* Close fullscreen */
+  function closeFullscreen() {
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    } else if (document.webkitExitFullscreen) { /* Safari */
+      document.webkitExitFullscreen();
+    } else if (document.msExitFullscreen) { /* IE11 */
+      document.msExitFullscreen();
+    }
+    video.classList.remove('video-fullscreen')
+  }
+
+let fullscreen = false
+
+// Toggle Fullscreen
+function toggleFullscreen() {
+    if (!fullscreen) {
+        openFullscreen(player)
+    } else {
+        closeFullscreen()
+    }
+    fullscreen = !fullscreen
+}
+
 // Event Listners
 playBtn.addEventListener('click', togglePlay)
 video.addEventListener('click', togglePlay)
@@ -128,3 +165,4 @@ progressRange.addEventListener('click', setProgress)
 volumeRange.addEventListener('click', changeVolume)
 volumeIcon.addEventListener('click', toggleMute)
 speed.addEventListener('change', changeSpeed)
+fullscreenBtn.addEventListener('click', toggleFullscreen)
